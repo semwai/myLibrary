cf017a7f0e8b4a22cd1abb3819f88071e42b4661
semwai <e14s@mail.ru>
2022-01-05 18:07:13 +0300

    move .py files to app folder

--- books.py
+++
@@ -1,3 +0,0 @@
-books = {1: ('/Users/semwai/Documents/books/Лутц Питон Том 1.pdf', 'Лутц питон том 1'),
-         2: ('/Users/semwai/Documents/books/Питон и крипта.pdf', 'Питон и крипта'),
-         }
