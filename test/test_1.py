b91a5d4610974249f7903318edb4dc06e8029cdd
semwai <e14s@mail.ru>
2022-01-06 20:36:54 +0300

    login test

--- test/test_1.py
+++ test/test_1.py
@@ -9,3 +9,11 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
+
+
+def test_login():
+    response_fail = client.post('/login', json={'username': 'test', 'password': 'tes1t'})
+    response_success = client.post('/login', json={'username': 'test', 'password': 'test'})
+    assert response_fail.json().get('access_token') is None
+    assert response_success.json().get('access_token') is not None
+
