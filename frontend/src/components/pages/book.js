dfb2f9743f7a87a68278b1aab4f85694dbd8dfd7
semwai <e14s@mail.ru>
2022-01-11 00:06:13 +0300

    fetch image with auth header

--- frontend/src/components/pages/book.js
+++ frontend/src/components/pages/book.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import useToken from '../useToken';
+
+function getPage(token, setter) {
+  fetch('http://localhost:8000/page/3', {
+    mode: 'cors',
+    headers: { 'Authorization': `Bearer ${token}` }
+  })
+  .then(res => res.blob())
+  .then(function (response) {
+    console.log(response)
+    var objectURL = URL.createObjectURL(response)
+    setter(objectURL)
+    console.log(objectURL)
+  })
+}
 
 export default function Book() {
-  return(
-    <h2>Book</h2>
+
+  const { token, setToken } = useToken();
+  const [page, setPage] = useState('');
+
+  useEffect(() => {
+    getPage(token, setPage)
+    
+  }, []);
+
+  return (
+    <div>
+
+       <p><img src={ page }/></p>
+    </div>
   );
 }
