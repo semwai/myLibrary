202a4c4fcf2cb30d37ba63e7a87c34640af7f48f
semwai <e14s@mail.ru>
2022-01-10 16:22:09 +0300

    add router support

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Book from './components/pages/book';
+import Index from './components/pages/Index';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />}>
+          </Route>
+          <Route path="/book" element={<Book />}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+
 
 export default App;
