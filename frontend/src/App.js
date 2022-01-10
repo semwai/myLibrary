a8441b2213c86ea557c444f1f37b664518311cd0
semwai <e14s@mail.ru>
2022-01-10 18:32:40 +0300

    login support for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Book from './components/pages/book';
 import Index from './components/pages/Index';
+import Login from './components/login';
+import useToken from './components/useToken';
+ 
 
 function App() {
+  const { token, setToken } = useToken();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
 
   return (
     <div className="wrapper">
---
+++ frontend/src/components/login.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './login.css';
+
+async function loginUser(credentials) {
+  let res = await fetch('http://localhost:8000/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+
+  let data = await res.json()
+  // console.log(data.access_token)
+  return data
+ }
+
+export default function Login({ setToken }) {
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const token = await loginUser({
+      username,
+      password
+    });
+    setToken(token);
+  }
+
+  return (
+    <div className="login-wrapper">
+      <h1>Please Log In</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Username</p>
+          <input type="username" onChange={e => setUserName(e.target.value)} />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" onChange={e => setPassword(e.target.value)} />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired
+}
