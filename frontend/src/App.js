eaa800dcf3b95166a769cdece4e8c799081619ae
semwai <e14s@mail.ru>
2022-01-10 20:15:28 +0300

    add router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,45 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import Book from './components/pages/book';
 import Books from './components/pages/books';
 import Index from './components/pages/Index';
 import Login from './components/login';
 import useToken from './components/useToken';
- 
+
 
 function App() {
   const { token, setToken } = useToken();
 
-  if(!token) {
+  if (!token) {
     return <Login setToken={setToken} />
   }
 
   return (
-    <div className="wrapper">
-      <h1>Application</h1>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />}>
-          </Route>
-          <Route path="/book" element={<Book />}>
-          </Route>
-          <Route path="/books" element={<Books />}>
-          </Route>
-        </Routes>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/book">book</Link>
+              </li>
+              <li>
+                <Link to="/books">books</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Index />}>
+            </Route>
+            <Route path="/book" element={<Book />}>
+            </Route>
+            <Route path="/books" element={<Books />}>
+            </Route>
+          </Routes>
+        </div>
       </BrowserRouter>
-    </div>
   );
 }
 
