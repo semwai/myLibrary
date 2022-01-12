55ea6a4b850cd1ed030537e7aa32a3f568537cd3
semwai <e14s@mail.ru>
2022-01-12 13:39:56 +0300

    `back` button and `404` error check

--- frontend/src/components/pages/book.js
+++ frontend/src/components/pages/book.js
@@ -7,7 +7,10 @@ import './book.css'
 function getPage(token, setPageUrl, setPage, book, page) {
 
   let path = `${process.env.REACT_APP_BACK_ADDR}/page/${book}`
-  if (page) {
+  if (page != null) {
+    console.log(page)
+    if (page < 0)
+      return
     path += `?page=${page}`
   }
   fetch(path, {
@@ -15,14 +18,19 @@ function getPage(token, setPageUrl, setPage, book, page) {
     headers: { 'Authorization': `Bearer ${token}`, 'page': '' }
   })
     .then(function (response) {
+      if (response.status == 404)
+        throw new Error(response.statusText);
       const page = parseInt(response.headers.get('page'))
-      // localStorage.setItem('page', page)
       setPage(page)
       return response.blob()
     })
     .then(function (blob) {
       var objectURL = URL.createObjectURL(blob)
       setPageUrl(objectURL)
+      window.scrollTo({
+        top: 0,
+        behavior: "instant"
+      });
     })
 }
 
@@ -44,9 +52,11 @@ export default function Book() {
       <p><img width='100%' src={pageUrl} /></p>
       <Container fluid>
         <Row>
-          <Col className='button-center element-center'>Back</Col>
-          <Col className='element-center'>{ page }</Col>
-          <Col className='button-center element-center' onClick={ () => {getPage(token, setPageUrl, setPage, book_id, page + 1) }}>
+          <Col className='button-center element-center' onClick={() => { getPage(token, setPageUrl, setPage, book_id, page - 1) }}>
+            Back
+          </Col>
+          <Col className='element-center'>{page}</Col>
+          <Col className='button-center element-center' onClick={() => { getPage(token, setPageUrl, setPage, book_id, page + 1) }}>
             Next
           </Col>
         </Row>
